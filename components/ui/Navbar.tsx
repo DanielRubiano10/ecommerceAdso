022ee5f4e5fb4792b50044ac4f41f28de77394c7
DanielRubiano <danii_95_crm@hotmail.com>
2024-03-26 17:47:12 -0500

    feat shopLayout and NavbarI

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,40 +1,47 @@
-import NextLink from 'next/link'
+import NextLink from "next/link";
 
-import { AppBar, Badge, Box, Button, IconButton, Link, Toolbar, Typography } from '@mui/material';
-import { SearchOutlined, ShoppingCartOutlined } from '@mui/icons-material';
-
+import {
+  AppBar,
+  Badge,
+  Box,
+  Button,
+  IconButton,
+  Link,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { SearchOutlined, ShoppingCartOutlined } from "@mui/icons-material";
 
 export const Navbar = () => {
   return (
     <AppBar>
       <Toolbar>
         {/* passHref para que se pase a nuestro componet link legacyBehavior para que sea compatible con Next12 */}
-        <NextLink href='/' passHref legacyBehavior>
-          <Link display='flex' alignItems='center'>
-            <Typography variant='h6'>Ecommerce |</Typography>
+        <NextLink href="/" passHref legacyBehavior>
+          <Link display="flex" alignItems="center">
+            <Typography variant="h6">Ecommerce |</Typography>
             <Typography sx={{ ml: 0.5 }}>Shop</Typography>
           </Link>
         </NextLink>
         <Box flex={1} />
         {/* styles de movil basado en los brekpoints de material*/}
-        <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-          <NextLink href='/category/hombre' passHref legacyBehavior>
+        <Box sx={{ display: { xs: "none", sm: "block" } }}>
+          <NextLink href="/category/hombre" passHref legacyBehavior>
             <Link>
               <Button>Hombre</Button>
             </Link>
           </NextLink>
-          <NextLink href='/category/mujer' passHref legacyBehavior>
+          <NextLink href="/category/mujer" passHref legacyBehavior>
             <Link>
               <Button>Mujere</Button>
             </Link>
           </NextLink>
-          <NextLink href='/category/unisex' passHref legacyBehavior>
+          <NextLink href="/category/unisex" passHref legacyBehavior>
             <Link>
-              <Button>Unisex</Button>
+              <Button>Productos</Button>
             </Link>
           </NextLink>
         </Box>
-
 
         <Box flex={1} />
 
@@ -45,17 +52,15 @@ export const Navbar = () => {
         <NextLink href="/cart" passHref legacyBehavior>
           <Link>
             <IconButton>
-              <Badge badgeContent={5} color="secondary">
+              <Badge badgeContent={2} color="error">
                 <ShoppingCartOutlined />
               </Badge>
             </IconButton>
           </Link>
         </NextLink>
 
-        <Button>
-          Menú
-        </Button>
+        <Button>Menú</Button>
       </Toolbar>
     </AppBar>
-  )
-}
+  );
+};
