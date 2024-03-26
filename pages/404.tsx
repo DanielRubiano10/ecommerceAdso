832bd5ca768870509c460615869035dfa84c3c10
DanielRubiano <danii_95_crm@hotmail.com>
2024-03-26 18:53:04 -0500

    feat shopLayout and NavbarI

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,7 +5,7 @@ const Custom404 = () => {
   return (
     <ShopLayout title='Pagina no encontrada' pageDescription='No hay nada que mostrar'>
       <Box display='flex' justifyContent='center' alignItems='center' height='calc(100vh - 200px)' sx={{ flexDirection: { xs: 'column', sm: 'row' } }}>
-        <Typography variant='h1' component='h1' fontSize={80} fontWeight={200}>404 |</Typography>
+        <Typography variant='h1' component='h1' fontSize={90} fontWeight={400}>404 |</Typography>
         <Typography marginLeft={2}>No se encuentra lo que estas buscando</Typography>
       </Box>
     </ShopLayout >
